132dd71128c70962572aa9a4897924cb8e017f4d
NarendraVarma87 <narendravarma97047@gmail.com>
2024-01-08 15:55:56 +0530

    day 14

--- Java8Features/StreamExample.java
+++ Java8Features/StreamExample.java
@@ -29,11 +29,22 @@ public class StreamExample {
         // Ascending Order
         list2.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList()).forEach(System.out::println);
 
-        System.out.println("**********************");
+        System.out.println("***********************");
+        // 2nd method
+        list2.stream().sorted((o1, o2) -> o1.compareTo(o2)).collect(Collectors.toList()).forEach(System.out::println);
 
+        System.out.println("**********************");
+        // 3rd method
+        list2.stream().sorted().collect(Collectors.toList()).forEach(System.out::println);
+
+        System.out.println("***********************");
         // Decending Order
         list2.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()).forEach(System.out::println);
-
+        System.out.println("***********************");
+        // 2 nd method
+        List<String> desc = list2.stream().sorted((o1, o2) -> o2.compareTo(o1)).collect(Collectors.toList());
+        desc.forEach(System.out::println);
+        System.out.println("***********************");
     }
 
 }
