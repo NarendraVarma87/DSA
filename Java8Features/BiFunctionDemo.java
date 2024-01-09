9a1a8e9723041f79fc22ba88c6fcadaae142d1ae
NarendraVarma87 <narendravarma97047@gmail.com>
2024-01-09 12:10:59 +0530

    day 15

---
+++ Java8Features/BiFunctionDemo.java
@@ -0,0 +1,18 @@
+package Java8Features;
+
+import java.util.function.BiFunction;
+
+/* it takes three input datatypes input1 datatype, input2 datatype , Result datatype */
+public class BiFunctionDemo {
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> biFunction = (t, u) -> (t + u);
+        BiFunction<Integer, Integer, Integer> substraction = (t, u) -> (t - u);
+        BiFunction<String, String, String> str = (t, u) -> (t.concat(u));
+        BiFunction<String, String, Boolean> str2 = (in1, in2) -> (in1.equals(in2));
+
+        System.out.println(biFunction.apply(130, 70));
+        System.out.println(substraction.apply(265, 123));
+        System.out.println(str.apply("Hello ", "World"));
+        System.out.println(str2.apply("narendra", "narendra"));
+    }
+}
